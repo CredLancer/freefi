5562b7ed24ee11f795d64189cb4b60da9df7f5c6
Julio M Cruz <julio.cruz@eb-ms.net>
2023-12-06 14:31:44 -0500

    Add Contract section

--- Frontend/app/add-contract/page.tsx
+++ Frontend/app/add-contract/page.tsx
@@ -1,13 +1,13 @@
 import FooterSectionWhite from "@/components/footer-section-white"
 import HeaderSection from "@/components/header-section"
-import PurposeSection from "@/components/purpose-section"
+import AddContractSection from "@/components/add-contract-section";
 
 export default function Page() {
     return (
         <>
             <HeaderSection />
             <div className="min-h-viewport-minus-sections">
-            <PurposeSection />
+                <AddContractSection />
             </div>
             <FooterSectionWhite />
         </>
